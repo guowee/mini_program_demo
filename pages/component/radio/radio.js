c09fd0914b5730944795e23ec9513a86e3fbcd06
Guowee <guoweegw@gmail.com>
2018-03-06 15:57:30 +0800

    add textarea radio slider picker label input form checkbox

--- pages/component/radio/radio.js
+++ pages/component/radio/radio.js
@@ -1,66 +1,25 @@
 // pages/component/radio/radio.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    items: [
+      { value: 'USA', name: '美国' },
+      { value: 'CHN', name: '中国', checked: 'true' },
+      { value: 'BRA', name: '巴西' },
+      { value: 'JPN', name: '日本' },
+      { value: 'ENG', name: '英国' },
+      { value: 'FRA', name: '法国' },
+    ]
   },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    var items = this.data.items;
+    for (var i = 0, len = items.length; i < len; ++i) {
+      items[i].checked = items[i].value == e.detail.value
+    }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    this.setData({
+      items: items
+    });
   }
 })
